430dbf1563c4a28edf964e05b0bee9e0ce4a2b59
hyunki0523 <159873023+hyunki0523@users.noreply.github.com>
2024-04-29 23:36:35 +0900

    product CRUD 완료 -권한 x-

---
+++ products/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("", views.ProductListAPIView.as_view(), name="product_list"),
+    path("<int:pk>/", views.ProductDetailAPIView.as_view(), name="product_detail"),
+
+]
